61488bc2ea0e117f61a158c0e5b81dd37c6e957f
William Zhang <77857686+william-v4@users.noreply.github.com>
2024-11-28 08:41:46 -0700

    animated base character walking

--- scripts/typingeffect.gd
+++ scripts/typingeffect.gd
@@ -13,7 +13,7 @@ func _process_custom_fx(charfx: CharFXTransform) -> bool:
 	# first, hide the character
 	charfx.visible = false
 	# for fading effect, change the opacity of the character based on the ratio of its TTL, weighted with speed, (elapsed_time - in sec) to its position in text (relative_index)
-	charfx.color.a = min(charfx.elapsed_time * speed/3 / charfx.relative_index, 1.0)
+	charfx.color.a = min(charfx.elapsed_time * speed/2 / charfx.relative_index, 1.0)
 	# then, if the character's TTL, weighted with the speed, meets the position of the character
 	if charfx.elapsed_time * speed >= charfx.relative_index:
 		# show the character
